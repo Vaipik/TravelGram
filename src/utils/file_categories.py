f96d311b989ff7a96500c94d80faa42a64889e1c
Vaipik <n.khylko@gmail.com>
2023-02-22 20:03:22 +0200

    utils file_categories

--- src/utils/file_categories.py
+++ src/utils/file_categories.py
@@ -2,6 +2,9 @@ from enum import Enum
 from pathlib import Path
 from typing import Optional
 
+from django.utils import timezone
+from django.db.models import FileField
+
 
 class Categories(Enum):
     """Allowed file categories and their extensions"""
@@ -9,10 +12,28 @@ class Categories(Enum):
     VIDEOS = ("mp4", "mov", "mkv")
 
 
-def determine_category(file: str) -> Optional[str]:
+def determine_category(extension: str) -> Optional[str]:
     """returns category name by file extension"""
-    extension = Path(file).suffix[1:]
 
     for category in Categories:
         if extension in category.value:
             return category.name
+
+
+def _get_folder_name(instance, filename: str) -> str:
+    """
+    This function is used with FileField to define folder name by file extension.
+    :param instance: FileField instance
+    :param filename: file which extension should be defined
+    :return: string with following format 'category/y/m/d/uuid.ext'
+    """
+    if type(instance) != FileField:
+        raise ValueError(f"{_get_folder_name.__name__} function must be used only with FileField")
+
+    extension = Path(filename).suffix[1:]
+    folder_name = determine_category(extension)
+    current_date = timezone.now().date()
+    new_filename = instance.pk + extension  # Changing filename to instance UUID
+    new_file_path = f"{folder_name}/{current_date}/{new_filename}"
+
+    return new_file_path
